8102f390bcd42e9a5a836e1f2a989ccacc09b884
afshanahmeda2k <afshanahmeda2k@gmail.com>
2021-08-25 16:04:16 +0530

    changed symlink to hardlink

--- autostart.sh
+++ autostart.sh
@@ -1,1 +1,11 @@
-../.dwm/autostart.sh
+#!/bin/sh
+#location of this file was ~/.dwm/autostart.sh
+
+# scripts
+/home/afshan/bin/change_wallpaper.sh
+
+while [[ : ]]; do
+  xsetroot -name "$(cat /sys/class/power_supply/BAT1/capacity) | $(date +'%a | %d %b %y | %I:%M %p')"
+  sleep 60
+done
+
