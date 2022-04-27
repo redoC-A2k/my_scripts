46941d5994d9ce03e769e6bae90d919b0eccc457
afshan ahmed khan <afshanahmeda2k@gmail.com>
2022-04-27 16:48:02 +0530

    added script for yt views increasing for assignment related purposes

---
+++ increase_yt_view.sh
@@ -0,0 +1,58 @@
+switchto=9
+playfor_min="30"
+playfor_max="42"
+actionafter_max="10"
+actionafter_min="3"
+wait_for_video_to_load="2"
+yt_url="https://www.youtube.com/watch?v=x5Nn9ACJ0vk"
+yt_video_title="Synchronization , thread communication , thread group in java - YouTube — Mozilla Firefox"
+actions=()
+get_win_id() {
+  win_id=$(wmctrl -l | grep "$yt_video_title"  | cut -d " " -f1 )
+  if [[ $win_id == "" ]]; then
+    firefox --new-window $yt_url &
+    firefox --new-window & #opening other window for doing other work
+    sleep 60s #wait for 1min for firefox to start
+    win_id=$(wmctrl -l | grep "$yt_video_title" | cut -d " " -f1 )
+  fi
+   win_id=$(echo $win_id | cut -d "x" -f2)
+   win_id=$((16#$win_id))
+  echo $win_id
+}
+
+get_stack_no() {
+  wins_in_stack=$(xdotool search --role browser)
+  echo $wins_in_stack>/dev/pts/2
+  declare -i i=0
+  for win in $wins_in_stack ; do
+    i=$i+1
+    if [[ $win -eq $1 ]]; then
+      break
+    fi
+  done
+  echo $i
+}
+
+while [[ 1 ]]; do
+  win_id=$(get_win_id)
+  stack_no=$(get_stack_no $win_id)
+  echo "%$stack_no"
+  xdotool search --role browser key --window "%$stack_no" ctrl+r
+  sleep $wait_for_video_to_load
+  xdotool search --role browser key --window "%$stack_no" space #start playing video 
+  time_to_play=$(($playfor_min+$(($RANDOM%$playfor_max))))
+  echo "will play for time $time_to_play"
+  SECONDS=0
+  while [[ 1 ]]; do
+    time_to_action=$(($actionafter_min+$(($RANDOM%$actionafter_max))))
+    rand_switch_to=$(($RANDOM%$switchto))
+    xdotool search --role browser key --window "%$stack_no" $rand_switch_to 
+    echo "$SECONDS and time to play $time_to_play\n"
+    if [[ $SECONDS -gt $time_to_play ]]; then
+      break
+    fi
+    echo "going to sleep before action\n"
+    sleep $time_to_action
+  done
+  echo "played for given time"
+done
