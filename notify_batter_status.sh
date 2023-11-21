4695b3a69be0ef68c02b6f93b80856d1050a66de
afshan ahmed khan <afshanahmeda2k@gmail.com>
2023-11-22 00:23:39 +0530

    Critical urgency level for low bettery in dunstify

--- notify_batter_status.sh
+++ notify_batter_status.sh
@@ -9,7 +9,7 @@ current_status=$(cat $base_dir/status)
 echo $current_status
 
 if [[ $current_capacity -lt 20 ]] && [[ $current_status == "Discharging" ]]; then
-  dunstify "Plug in your charger" "Battery is less than $current_capacity"
+  dunstify -u critical "Plug in your charger" "Battery is less than $current_capacity"
 fi
 
 if [[ $current_capacity -gt 93 ]] && [[ $current_status == "Charging" ]]; then
