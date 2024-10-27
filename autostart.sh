47411fe6e92b245c9b3f96df65f6d1e1e2030754
afshan ahmed khan <afshanahmeda2k@gmail.com>
2024-10-27 14:27:35 +0530

    swap capslock and control key for both logitech and inbuilt keyboard

--- autostart.sh
+++ autostart.sh
@@ -8,6 +8,16 @@
 #   echo "Already running pywall script" &> output.txt
 # fi
 
+# /usr/bin/setxkbmap -device 13 -option "ctrl:swapcaps"
+# /usr/bin/setxkbmap -device 18 -option "ctrl:swapcaps"
+logitechKeyboardId=$(xinput -list | grep -E "(Logitech Wireless Keyboard).*(slave[ ]+keyboard)" | awk '{print $6}' | awk -F '=' '{print $2}')
+# echo "logitech keyboard id = $logitechKeyboardId"
+acerKeyboardId=$(xinput -list | awk '/AT Translated Set 2 keyboard/ {print $7}' | cut -d '=' -f 2)
+# echo "acer keyboard id = $acerKeyboardId"
+
+/usr/bin/setxkbmap -device $acerKeyboardId -option "ctrl:swapcaps"
+/usr/bin/setxkbmap -device $logitechKeyboardId -option "ctrl:swapcaps"
+
 while [[ 1 ]]; do
   bat=🔋
   if [[ $(acpi -b | cut -d":" -f2 | cut -d"," -f1) == " Charging" ]]; then
@@ -15,7 +25,10 @@ while [[ 1 ]]; do
   fi
 
   conn=🔗
-  conn_ip=$(ifconfig | grep -A 1 "wlp3s0" | tail -1 | grep "inet" |  cut -d" " -f10) 
+  conn_ip=$(ifconfig | grep -A 1 "wlp3s0" | tail -1 | grep "inet" |  cut -d" " -f10)
+  if [[ -z $conn_ip ]] ; then
+    conn_ip=$(ifconfig | grep -A 1 "bnep0" | tail -1 | grep "inet" |  cut -d" " -f10)
+  fi
   if [[ -z $conn_ip ]] ; then
     conn=🚫
     conn_ip="Not connected"
@@ -23,7 +36,12 @@ while [[ 1 ]]; do
 
   mem="$(free --mega | grep "Mem" | awk '{print $3}') Mb"
 
-  xsetroot -name " $bat $( acpi -b | cut -d":" -f2 | cut -d"," -f2) | 🔰 $(date +'%A') | 📆 $(date +'%d %b %y') | ⏰ $(date +'%I:%M %p') | $conn $conn_ip | 💾 used:$mem "
-  sleep 20
+  # echo $conn $conn_ip;
+  xsetroot -name " $bat $( acpi -b | cut -d":" -f2 | cut -d"," -f2) | 🔰 $(date +'%A') | 📆 $(date +'%d %b %y') | ⏰ $(date +'%I:%M %p') | $conn $conn_ip | 💾 used: $mem "
+  # xsetroot -name "afshan"
+
+  /home/afshan/my_scripts/notify_batter_status.sh
+
+  sleep 10
 done
 
